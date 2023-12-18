fed04d487f927fb97bc1c3fe04b0f1fd88e1094e
liuqingdi <liuqingdi907@gmail.com>
2023-12-18 15:58:12 +0800

    forms finished

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
-from flask import Flask#从flask包中导入Flask类
+from flask import Flask
+from config import Config
 
 app = Flask(__name__)
-
-print('等会谁（哪个包或模块）在使用我：',__name__)
+app.config.from_object(Config)
+# print('等会谁（哪个包或模块）在使用我：',__name__)
+print(app.config['SECRET_KEY'])
 
 
 #从app包中导入模块routes
