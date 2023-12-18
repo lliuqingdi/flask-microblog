6d7aa6fa2bf2c7ca12c5236098c8e20066291169
liuqingdi <liuqingdi907@gmail.com>
2023-12-18 17:04:39 +0800

    forms finished secondly

--- app/routes.py
+++ app/routes.py
@@ -3,10 +3,10 @@ from flask import render_template, flash, redirect, url_for
 from app import app
 
 from app.forms import LoginForm
+
 #2个路由
 @app.route('/')
 @app.route('/index')
-#1个视图函数
 def index():
 	user = {'username': 'Yuyu'}
 	# 创建一个列表：帖子。里面元素是两个字典，每个字典里元素还是字典，分别作者、帖子内容。
@@ -23,10 +23,10 @@ def index():
 	return render_template('index.html',user=user,posts=posts)
 
 
-@app.route('/login',methods=['GET','POST'])
+@app.route('/log',methods=['GET','POST'])
 def login():
 	form = LoginForm()
 	if form.validate_on_submit():
 		flash('Login requested for user {},remember_me={}'.format(form.username.data,form.remember_me.data))
-		return redirect(url_for('index'))
+		return redirect('/index')
 	return render_template('login.html',title='Sign In',form=form)
