98b953bf16e22d15cacdb40ba07655e27227b31e
liuqingdi <liuqingdi907@gmail.com>
2023-12-19 20:23:08 +0800

    database

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,19 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+#数据库对象
+db = SQLAlchemy(app)
+#迁移引擎对象
+migrate = Migrate(app, db)
+
 # print('等会谁（哪个包或模块）在使用我：',__name__)
-print(app.config['SECRET_KEY'])
+# print(app.config['SECRET_KEY'])
 
 
 #从app包中导入模块routes
-from app import routes
+from app import routes,models
