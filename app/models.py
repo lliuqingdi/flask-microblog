77687bba9571d2e2c92d7634f322a6ed52097a4d
liuqingdi <liuqingdi907@gmail.com>
2024-11-28 19:49:52 +0800

    file: 格式化模型代码缩进

--- app/models.py
+++ app/models.py
@@ -7,15 +7,18 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from hashlib import md5
 
+
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
+
 
 followers = db.Table(
     'followers',
     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
     db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
-    )
+)
+
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -79,6 +82,7 @@ class User(UserMixin, db.Model):
             followers.c.follower_id == self.id)
         own = Post.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Post.timestamp.desc())
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
